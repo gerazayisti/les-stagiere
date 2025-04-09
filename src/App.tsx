8dd0feb96f3449192f3fb00a5dac4799b6feed68
Gervais <gervais.care2data@gmail.com>
2025-04-10 00:06:19 +0100

    fix them to properly point to the "Mentions légales" and "Politique de confidentialité"

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,26 @@ import MesCandidatures from "./pages/MesCandidatures";
 import NotFound from "./pages/NotFound";
 import MentionsLegales from "./pages/MentionsLegales";
 import Confidentialite from "./pages/Confidentialite";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="theme-preference">
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/inscription" />} />
-          <Route path="/inscription" element={<Inscription />} />
-          <Route path="/connexion" element={<Connexion />} />
-          <Route path="/stagiaires/:id" element={<ProfilStagiaire />} />
-          <Route path="/entreprises/:id" element={<ProfilEntreprise />} />
-          <Route path="/profil-enrichi" element={<ProfilEnrichi />} />
-          <Route path="/mes-candidatures" element={<MesCandidatures />} />
-          <Route path="/mentions-legales" element={<MentionsLegales />} />
-          <Route path="/confidentialite" element={<Confidentialite />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Navigate to="/inscription" />} />
+            <Route path="/inscription" element={<Inscription />} />
+            <Route path="/connexion" element={<Connexion />} />
+            <Route path="/stagiaires/:id" element={<ProfilStagiaire />} />
+            <Route path="/entreprises/:id" element={<ProfilEntreprise />} />
+            <Route path="/profil-enrichi" element={<ProfilEnrichi />} />
+            <Route path="/mes-candidatures" element={<MesCandidatures />} />
+            <Route path="/mentions-legales" element={<MentionsLegales />} />
+            <Route path="/confidentialite" element={<Confidentialite />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
         </Routes>
       </Router>
       <Toaster position="top-center" />
